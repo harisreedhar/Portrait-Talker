38b03c59d6ee3223d85715891e7c402fc8ba7140
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2023-12-08 22:35:35 +0530

    Fix memory usage
    - removed video loading to memory
    - use ffhq alignement for face crop

--- run_video_driven.py
+++ run_video_driven.py
@@ -33,28 +33,12 @@ def paste_back(img, face, matrix):
 def process_source(model, img_path, size, crop_scale=1.6):
     ori_img = cv2.imread(img_path)
     bboxes, kpss = model.detect(ori_img, det_thresh=0.6)
+    assert len(kpss) != 0, "No face detected"
     aimg, mat = get_cropped_head(ori_img, kpss[0], size=size, scale=crop_scale)
     aimg = np.transpose(aimg[:,:,::-1], (2, 0, 1)) / 255
     aimg_tensor = torch.from_numpy(aimg).unsqueeze(0).float()
     aimg_tensor_norm = (aimg_tensor - 0.5) * 2.0
     return aimg_tensor_norm, ori_img, mat
-
-
-def process_driving(vid_path, size=256):
-    cap = cv2.VideoCapture(vid_path)
-    video_fps = cap.get(cv2.CAP_PROP_FPS)
-    video = []
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if ret:
-            frame = cv2.resize(frame, (size, size))
-            video.append(frame)
-        else:
-            break
-    cap.release()
-    vid = torch.from_numpy(np.asarray(video)).permute(0, 3, 1, 2).unsqueeze(0)
-    vid_norm = (vid / 255.0 - 0.5) * 2.0
-    return vid_norm, video_fps
 
 
 class Demo(nn.Module):
@@ -74,31 +58,42 @@ class Demo(nn.Module):
         print('[ loading data ]')
 
         self.source, self.original_image, self.matrix = process_source(detector, args.source_path, self.size, crop_scale=args.crop_scale)
-        self.driving, self.fps = process_driving(args.driving_path, size=self.size)
-
         self.source = self.source.to(self.device)
-        self.driving = self.driving.to(self.device)
 
     def run(self):
         print('[ running ]')
+
+        cap = cv2.VideoCapture(args.driving_path)
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
         h, w = self.original_image.shape[:2]
         os.makedirs(args.save_dir, exist_ok=True)
         save_path = os.path.join(args.save_dir, 'video_driven_result.avi')
-        out = cv2.VideoWriter(save_path, fourcc, int(self.fps), (w,h))
+        out = cv2.VideoWriter(save_path, fourcc, int(fps), (w,h))
 
         with torch.no_grad():
-            h_start = self.driving[:, 0, :, :, :].to(self.device)
-            h_start = self.gen.enc.enc_motion(h_start)
-            for i in tqdm(range(self.driving.size(1))):
-                img_target = self.driving[:, i, :, :, :].to(self.device)
-                img_recon = self.gen(self.source, img_target, h_start)
-                np_img_recon = img_recon.permute(0,2,3,1).cpu().numpy()[0]
-                np_img_recon = ((np_img_recon[:,:,::-1] + 1) / 2) * 255
-                pasted = paste_back(self.original_image, np_img_recon, self.matrix)
-                out.write(pasted.astype('uint8'))
+            h_start = None
+            for index in tqdm(range(total_frames), desc='[ Processing frames ]', unit='frame'):
+                ret, frame = cap.read()
+                if ret:
+                    frame = cv2.resize(frame, (self.size, self.size))
+                    frame_np = np.expand_dims(frame, axis = 0).astype('float32')
+                    frame_np = (frame_np / 255 - 0.5) * 2.0
+                    frame_np = frame_np.transpose(0, 3, 1, 2)
+                    frame_torch = torch.from_numpy(frame_np).to(self.device)
+                    if index == 0:
+                        h_start = self.gen.enc.enc_motion(frame_torch)
+                    frame_recon = self.gen(self.source, frame_torch, h_start)
+                    frame_recon_np = frame_recon.permute(0, 2, 3, 1).cpu().numpy()[0]
+                    frame_recon_np = ((frame_recon_np[:,:,::-1] + 1) / 2) * 255
+                    pasted = paste_back(self.original_image, frame_recon_np, self.matrix)
+                    out.write(pasted.astype('uint8'))
+                else:
+                    break
 
+        cap.release()
         out.release()
         cv2.destroyAllWindows()
 
@@ -108,7 +103,7 @@ if __name__ == '__main__':
     parser.add_argument("--source_path", type=str, default='')
     parser.add_argument("--driving_path", type=str, default='')
     parser.add_argument("--save_dir", type=str, default='./test/result')
-    parser.add_argument("--crop_scale", type=float, default=1.5)
+    parser.add_argument("--crop_scale", type=float, default=1.25)
     args = parser.parse_args()
 
     demo = Demo(args)
